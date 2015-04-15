fb413eaeda0d191b7d6cdf3b1ca1092f35e64bb8
Janay Peters <japeters15@gmail.com>
2015-04-15 15:23:32 -0400

    Prompt and Alert
    Creating an alert instead of prompt

--- Assignment[Values_Variables]/js/script.js
+++ Assignment[Values_Variables]/js/script.js
@@ -3,4 +3,7 @@
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter width");
 var height = prompt("Please enter height");
 var area = width * height;//calculates with that information
-console.log(area);//prints out to console
+//console.log(area);//prints out to console
+
+alert("the area of your rectangle is " + area + "sq feet");
+//or var result = ^... alert(result);
