8a4f872f774452f1cdce0e15d23034ebfd492704
Janay Peters <japeters15@gmail.com>
2015-04-15 15:19:58 -0400

    Prompt and Alert
    Prompting the user with Width and Height

--- Assignment[Values_Variables]/js/script.js
+++ Assignment[Values_Variables]/js/script.js
@@ -1,8 +1,6 @@
-//Expressions - Assignment Operators
+//Prompt and Alert
 
-var a = 3;
-a = a + 4; //or a += 4 (Add), a*= 4 (Multiply), a /= 4 (Divide), a -= 4 (Subtract)
-a ++ ; //a = a + 1 // a += 1
-a --; //a = a - 1
-console.log(a);
-
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter width");
+var height = prompt("Please enter height");
+var area = width * height;//calculates with that information
+console.log(area);//prints out to console
