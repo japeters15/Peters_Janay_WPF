0ece6d911bdd555bbbebbeb3074ac78f6af174e9
Janay Peters <japeters15@gmail.com>
2015-04-15 14:23:14 -0400

    Expressions- Concatenating Strings
    Stringing items together

--- Assignment[Values_Variables]/js/script.js
+++ Assignment[Values_Variables]/js/script.js
@@ -1,6 +1,7 @@
-//Expressions - Expressions with Arrays
+//Expressions - Concatenating Strings
 
-var orangeBins = [234, 567, 883];
+var firstName = "Janay";
+var lastName = "Peters";
+var fullName = firstName + " " + lastName;
 
-var total = orangeBins[0]+ orangeBins[1] + orangeBins[2];
-console.log(total);
+console.log(fullName);
