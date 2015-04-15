8e9e854ffacb3f585e1719fa17294d167e23141e
Janay Peters <japeters15@gmail.com>
2015-04-15 13:57:21 -0400

    Expressions
    Adding and defining

--- Assignment[Values_Variables]/js/script.js
+++ Assignment[Values_Variables]/js/script.js
@@ -1,10 +1,6 @@
-// Arrays
+// Expressions
 
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+var a = 2; //sets up variable a and defines it with 2
+a = a + 3; //add 3 to a
+console.log(a);
 
-muppetNames[1] = "Rizzo"; //Change index #1 to Rizzo
-muppetNames[2] = "Fozzy";
-
-var num = 2;
-
-console.log(muppetNames[num]); //lets me see whats in index #1
