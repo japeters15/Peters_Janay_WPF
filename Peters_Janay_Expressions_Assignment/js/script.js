ff5cc1e25d2eb320f5f92f021fedd2a714c3b93c
Janay Peters <japeters15@gmail.com>
2015-04-16 08:07:53 -0400

    Expressions Assignment
    added height

--- Peters_Janay_Expressions_Assignment/js/script.js
+++ Peters_Janay_Expressions_Assignment/js/script.js
@@ -3,5 +3,7 @@ Janay Peters
 Expressions Assignment
 04/15/2015
  */
- alert("Test to see if connected");
+
+var height = prompt("I am calculating the surface area of a home. \nPlease enter the height");
+
 
