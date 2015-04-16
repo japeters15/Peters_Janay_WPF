1d0df9dbdd00cbd215ea409c8bafad19518eef10
Janay Peters <japeters15@gmail.com>
2015-04-16 08:32:33 -0400

    Expressions Assignment
    Added alerts for user to see calculation

--- Peters_Janay_Expressions_Assignment/js/script.js
+++ Peters_Janay_Expressions_Assignment/js/script.js
@@ -10,8 +10,24 @@ var area = height * width;
 
 console.log(area);
 
+alert("the area of room 1 is"+ area);
+
 var height2 =  prompt("Please enter height of room 2");
 var width2 = prompt("Please enter width of room 2");
+
+var area2 = height2 * width2;
+
+console.log(area2);
+
+var height3 = prompt("Please enter height of room 3");
+var width3 = prompt("Please enter width of room 3");
+
+var area3 = height3 * width3;
+
+console.log(area3);
+
+
+
 
 
 
