8ef912b44bd737ed450b38d463a0176a313e3509
Janay Peters <japeters15@gmail.com>
2015-04-16 14:26:12 -0400

    Expressions Worksheet- Dog years
    Added ending alert

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -8,7 +8,8 @@ alert ("Keep in mind! 1 year in human years = 7 years in dog years.");
 var humanAge = prompt ("How old is the dog in human years?"); //Sparky is 2 years old
 var dogYear = 7; //One year for us is 7 years for them.
 
-alert ("Sparky is" + " " + humanAge * dogYear + " " + "years old in dog years");
+alert ("Sparky is" + humanAge + " " + "in human years which is " + " " + humanAge * dogYear + " " + "years old in dog years");
+
 
 
 
