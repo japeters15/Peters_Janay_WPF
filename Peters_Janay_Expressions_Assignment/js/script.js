a937b05e5f6c60bd1ddacf6afbe591d9b36e79cf
Janay Peters <japeters15@gmail.com>
2015-04-16 08:15:01 -0400

    Expressions Assignment
    Made changes to the problem

--- Peters_Janay_Expressions_Assignment/js/script.js
+++ Peters_Janay_Expressions_Assignment/js/script.js
@@ -4,8 +4,8 @@ Expressions Assignment
 04/15/2015
  */
 
-var height = prompt("I am calculating the surface area of a home. \nPlease enter the height");
-var width = prompt("Please enter the width");
+var height = prompt("I am calculating the surface area of a home. \nPlease enter the height of room 1");
+var width = prompt("Please enter the width of room 1");
 var surfaceArea = height * width;
 
 console.log(surfaceArea);
