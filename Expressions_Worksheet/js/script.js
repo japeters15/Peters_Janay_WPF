ba323a98265228fd7c3235081d3231fc82574a9b
Janay Peters <japeters15@gmail.com>
2015-04-16 19:43:30 -0400

    Expressions Worksheet- Slice of pie 1
    Included the number or people and the calculations

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -5,13 +5,15 @@
  */
 
 
-var people = prompt ("How many people are at your party?");
-var pizzas = prompt ("How many slices of pizza are there?"); 
-var slices = (people * pizzas);
+var people = prompt ("How many people are at your party?");//12 people at my party
+var pizzas = prompt ("How many pizzas are there?");//5 pizzas
+var slices = (pizzas*8/people);
+ alert ("The number of slices per person is" + " " + slices); //total is 3 slices per person
 
 console.log(slices);
-alert ("pizzas are cut into 8 slices")
-alert ("Sparky is" + " " + humanAge + " " + "in human years which is " + " " + humanAge * dogYear + " " + "years old in dog years");
+
+
+
 
 
 
