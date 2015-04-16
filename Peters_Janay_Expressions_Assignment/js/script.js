5810c7a4aba3e29e8551a5fc62f9a2e7603f53b7
Janay Peters <japeters15@gmail.com>
2015-04-16 08:52:40 -0400

    Expressions Assignment
    Added the spaces for the answer and added name prompt

--- Peters_Janay_Expressions_Assignment/js/script.js
+++ Peters_Janay_Expressions_Assignment/js/script.js
@@ -4,13 +4,14 @@ Expressions Assignment
 04/15/2015
  */
 
-var height = prompt("I am calculating the surface area of a home. \nPlease enter the height of room 1");
+var info = prompt("I am calculating the surface area of your home. \nPlease enter your name");
+var height = prompt("Please enter the height of room 1");
 var width = prompt("Please enter the width of room 1");
 var area = height * width;
 
 console.log(area);
 
-alert("the area of room 1 is"+ area);
+alert("the area of room 1 is"+ " " + area);
 
 var height2 =  prompt("Please enter height of room 2");
 var width2 = prompt("Please enter width of room 2");
@@ -19,7 +20,7 @@ var area2 = height2 * width2;
 
 console.log(area2);
 
-alert("the area of room 2 is"+ area2);
+alert("the area of room 2 is"+ " "+ area2);
 
 var height3 = prompt("Please enter height of room 3");
 var width3 = prompt("Please enter width of room 3");
@@ -28,7 +29,7 @@ var area3 = height3 * width3;
 
 console.log(area3);
 
-alert("the area of room 3 is"+ area3);
+alert("the area of room 3 is"+ " " +area3);
 
 var answer = [area, area2, area3];
 
