0683ad285e2fd6843d2ad24a1ab4158f5af7bde9
Janay Peters <japeters15@gmail.com>
2015-04-16 07:54:51 -0400

    Expressions Worksheet
    Set up of first problem

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Janay Peters
+ * Expressions Worksheet
+ * 04/15/2015
+ */
+
+var humanAge = 2; //Sparky is 2 years old
+var dogYear = 7; //One year for us is 7 years for them.
+
+var actualAge = humanAge * dogYear;
+
+console.log(actualAge);
+
+
