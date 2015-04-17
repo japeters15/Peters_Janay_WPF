07b7279ae436a72e3726d99ccfdc3e2fa8818765
Janay Peters <japeters15@gmail.com>
2015-04-16 20:04:36 -0400

    Expressions Worksheet- Slice of Pie 2
    Gave the total and sum of how much pizza sparky gets to eat

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -5,12 +5,19 @@
  */
 
 
-var people = prompt ("How many people are at your party?");//12 people at my party
-var pizzas = prompt ("How many pizzas are there?");//5 pizzas
-var slices = (pizzas*8/people);
- alert ("The number of slices per person is" + " " + slices); //total is 3 slices per person
+alert ("There are 12 people at your party");//12 people at my party
+alert ("There are 5 pizzas total");//5 pizzas
+var slices = (5*8/12);
 
+ alert ("The number of slices per person is" + " " + slices); //total is 3 slices per person
 console.log(slices);
+
+var sparky = (.3 * 12);
+
+console.log(sparky);
+
+
+alert ("Sparky got" + " " + sparky + " " + "slice of pizza");
 
 
 
