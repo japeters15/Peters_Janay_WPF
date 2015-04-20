95ebad76731f34470e9b2a68b4c8a30ac9184ef4
Janay Peters <japeters15@gmail.com>
2015-04-20 08:52:26 -0400

    Conditionals
    printed information to web source

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,4 +5,6 @@ var oldEnough = true;
 //if the child is old enough, print to the console "you can ride."
 if(oldEnough){
     //code performed if condition is true
+    console.log("You can ride the coaster!");
 }
+console.log("What comes after");
