ade6ad8cfbe66b20f6dff439671bc82d25ab51c0
Janay Peters <japeters15@gmail.com>
2015-04-21 12:39:27 -0400

    Conditional Logic - Logical Operations
    inserted paycheck greater then price of budget, but we get two print
    outs.

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,7 +5,12 @@ var iPhonePrice = 199.99;
 var payCheck = 200;
 
 //if the price of the phone is less than our budget and if out paycheck is over 300
-if(iPhonePrice < budget){
+if(iPhonePrice < budget & payCheck > 300){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!");
+}
+if(payCheck > 300){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
