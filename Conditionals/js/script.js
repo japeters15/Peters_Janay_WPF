a965482af6d2e8c66a8b597badeef57b726a42ea
Janay Peters <japeters15@gmail.com>
2015-04-21 13:53:59 -0400

    Conditional Logic - Logical Operations
    Added a true expression

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -2,10 +2,10 @@
 
 var budget = 300;
 var iPhonePrice = 199.99;
-var payCheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than our budget and if out paycheck is over 300
-if(iPhonePrice && payCheck < 300){
+if(iPhonePrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
