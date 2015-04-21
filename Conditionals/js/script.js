2386630f022dd46dda6de70a63db2b6c1983e893
Janay Peters <japeters15@gmail.com>
2015-04-21 13:39:03 -0400

    Conditional Logic - Logical Operations
    Added the “&&” operator and set the iPhone price and paycheck about to
    less than 300 (my budget)

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,12 +5,7 @@ var iPhonePrice = 199.99;
 var payCheck = 200;
 
 //if the price of the phone is less than our budget and if out paycheck is over 300
-if(iPhonePrice < budget & payCheck > 300){
-    console.log("We can buy the phone!");
-}else{
-    console.log("No phone for you!");
-}
-if(payCheck > 300){
+if(iPhonePrice && payCheck < 300){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
