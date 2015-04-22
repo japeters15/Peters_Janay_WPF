95692b299803042d8b56e9305ac8d4acc3f1fed3
Janay Peters <japeters15@gmail.com>
2015-04-22 13:27:27 -0400

    Conditionals Assignment
    Decided on my topic, Night on the town! Inserted my first prompt,
    budget.

---
+++ Peters_Janay_Conditionals_Assignment/js/script.js
@@ -0,0 +1,12 @@
+//Janay Peters
+// 04/22/2015
+// Conditionals Assignment
+
+//You and friends are going out for a night on the town, but you have to decide what size vehicle to take,
+//and your budget for the night.
+
+alert ("You are going out with friends tonight and we have to figure out what size vehicle to take and set up your budget!");
+
+var payCheck= prompt ("Lets start with your budget! How much did you make on your pay check?");
+
+
