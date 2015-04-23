f22ca9aa02c6d6064c68f9d839e938dc60e64c95
Janay Peters <japeters15@gmail.com>
2015-04-23 12:49:48 -0400

    Conditionals Worksheet
    Set up my prompts and printing them to the console

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,4 +1,14 @@
 // Janay Peters
 // 04/23/2015
-// Conditional Worksheet
+// Conditional Worksheet- Last Chance for Gas
+
+alert ("Lets see if you can make it across the desert with the gas you have in your tank or if you need to fill up now!");
+
+var efficient = prompt ("What is the gas efficiency of your car?");
+
+var reading = prompt ("How much gas (in percent) do you have left in your tank? ");
+
+var tank = prompt ("How many gallons is you gas tank capacity?");
+
+
 
