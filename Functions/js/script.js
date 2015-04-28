de338d496a80f2155a7fd2bf4835528432deaeab
Janay Peters <japeters15@gmail.com>
2015-04-28 12:27:45 -0400

    Functions - Anonymous Functions
    Commented labels (Defining, and Invoking)

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,11 +1,11 @@
 // Functions - Anonymous Functions (closures)
 
 
-var calcArea = function(width , height){
+var calcArea = function(width , height){ //defining
     //code the function runs
     var area = width * height;
     return area;
 }
 
-var a= calcArea(20,30);
+var a= calcArea(20,30); //invoking
 console.log(a);
