6560db7c7ca74279332958d8c874e80b95b2c729
Janay Peters <japeters15@gmail.com>
2015-04-28 13:31:40 -0400

    Functions Worksheet- Circumference
    Created my function and printed it to console

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,11 @@
+// Janay Peters
+// 04/28/2015
+// Functions Worksheet- Circumference
+
+circumference(3.14, 2);
+
+function circumference(pie, radius){
+    var circ = 2 * pie * radius;
+    console.log(circ);
+}
+
