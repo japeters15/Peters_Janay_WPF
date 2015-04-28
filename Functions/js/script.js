3bc19745b0d1fd7b94b75402f9df5dc6372629ac
Janay Peters <japeters15@gmail.com>
2015-04-28 12:18:19 -0400

    Functions - Function vs. Procedure
    Listed the difference between a function and a procedure.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,9 +1,14 @@
-// Functions - Returning Values
+// Functions - Function vs. Procedure
 
-var total = calcArea(30, 20);
 
-function calcArea(w,h){
-    var area = w*h;
-    return area;//console.log(area); //function spit!
+//This is a function
+function calcArea(width, height){
+    var area = width *height;
+    return area;
 }
-console.log(total);
+
+//This is a procedure (list of things to do)
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area); //no return
+}
