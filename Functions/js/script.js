3cc2a9c0589093a80a8d61636c3995f8f2f28ed0
Janay Peters <japeters15@gmail.com>
2015-04-28 09:02:58 -0400

    Functions - Variable Scape
    Added a function outside of the (calcArea) function to how to functions
    with the same name are separated.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,15 +1,14 @@
-// Functions - Function Execution
+// Functions - Variable Scape
 
-function outptMsg(){
-    console.log("Hello World");
-}
+var width = 5;
+
 
 function calcArea(){ // Function Definition
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log (area);
+    //console.log (area);
 }
-
+console.log(width);
 calcArea(); //Invocation (Function call)
 
