9de4b9915f27744b20b06a10baffa74041d243b3
Janay Peters <japeters15@gmail.com>
2015-04-29 11:59:16 -0400

    Functions Assignment
    made my first function and ended up going with a gas mileage problem!

--- Peters_Janay_Functions Assignment/js/script.js
+++ Peters_Janay_Functions Assignment/js/script.js
@@ -3,3 +3,15 @@
 // Functions Assignment
 
 
+var car1 = prompt ("Hello, I an conducting a study about gas mileage! Lets say that you have two cars with full tanks of gas, we have to figure out which one could go the furthest distance! What size gallon tank does your first car have?");
+
+var mile = prompt ("What are the average miles per gallon?")
+
+var total = mpg (car1, mile);
+
+function mpg(m, g){
+    var distance = m * g;
+    return distance;
+}
+
+console.log(total);
