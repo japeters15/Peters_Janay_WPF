99835b96c2caf26bd391cc6dd7207487bd27eb55
Janay Peters <japeters15@gmail.com>
2015-04-29 08:28:09 -0400

    Loops
    Added my comments and learned how to set up a while loop

--- Loops/js/script.js
+++ Loops/js/script.js
@@ -1,7 +1,8 @@
 console.log("---------Loops-----------");
 
-var b = 10;
+var b = 50; // sets up the index
 
-while (b > 0){
-    console.log(b);
+while (b > 0){ //check the condition
+    console.log(b + "kegs on the wall");
+    b--; //increments or decrements the index
 }
