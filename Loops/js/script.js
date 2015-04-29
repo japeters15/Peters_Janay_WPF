26acc6f3cd1a2622ebb3054075b10d8b1abb0b4c
Janay Peters <japeters15@gmail.com>
2015-04-29 08:30:30 -0400

    Do while loops
    Added a second loop after the first one

--- Loops/js/script.js
+++ Loops/js/script.js
@@ -6,3 +6,13 @@ while (b > 0){ //check the condition
     console.log(b + "kegs on the wall");
     b--; //increments or decrements the index
 }
+
+console.log("------Do while loops");
+
+
+var c = 10;
+
+do {
+    console.log(c + "kegs on the wall");
+}
+while (c > o);
